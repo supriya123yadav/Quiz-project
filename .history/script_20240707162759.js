08ce0272516de93578f9533a73b1f092a2068d4c
c.node-project <supriyakancham@gmail.com>
2024-07-12 08:57:39 +0530

    first commit

---
+++ .history/script_20240707162759.js
@@ -0,0 +1,14 @@
+const questions = [
+    {
+       question: "Which is the largest animal in the world?",
+       answers: [
+        { text: "Shark", correct: false}
+        { text: "Blue Whale", correct: false}
+        { text: "Elephant", correct: false}
+        { text: "Giraffe", correct: false}
+        
+       ]
+
+
+    }
+]
---
+++ .history/script_20240707164659.js
@@ -0,0 +1,74 @@
+const questions = [
+    {
+       question: "Which is the largest animal in the world?",
+       answers: [
+        { text: "Shark", correct: false},
+        { text: "Blue Whale", correct: true},
+        { text: "Elephant", correct: false},
+        { text: "Giraffe", correct: false},
+        
+       ]
+    },
+    {
+        question: "Which is the smallest country in the world?",
+        answers: [
+         { text: "Vatican City", correct: true},
+         { text: "Bhutan", correct: false},
+         { text: "Nepal", correct: false},
+         { text: "Sri Lanka", correct: false},
+         
+        ]
+     },
+     {
+        question: "Which is the largest desert in the world?",
+        answers: [
+         { text: "Kalahari", correct: false},
+         { text: "Gobi", correct: false},
+         { text: "Sahara", correct: false},
+         { text: "Antarctica", correct: true},
+         
+        ]
+     },
+     {
+        question: "Which is the smallest continent in the world?",
+        answers: [
+         { text: "Asia", correct: false},
+         { text: "Australia", correct: true},
+         { text: "Arctic", correct: false},
+         { text: "Africa", correct: false},
+         
+        ]
+     }
+
+];
+
+const questionElement = document.getElementById("question");
+const answerButton = document.getElementById("answer-buttons");
+const nextButton = document.getElementById("next-btn");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
